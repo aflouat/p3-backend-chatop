065acd5a4dd4873ce5e4696efdf07c91870323e0
aflouat <aflouat@gmail.com>
2024-08-16 10:41:51 +0200

    Initial commit

---
+++ src/main/java/fr/tmsconsult/p3_backend_chatop/P3BackendChatopApplication.java
@@ -0,0 +1,13 @@
+package fr.tmsconsult.p3_backend_chatop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P3BackendChatopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P3BackendChatopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/tmsconsult/p3_backend_chatop/P3BackendChatopApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.tmsconsult.p3_backend_chatop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P3BackendChatopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
